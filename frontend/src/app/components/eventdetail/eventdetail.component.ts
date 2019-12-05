09d6eec8f8afe78136e83b8a5216b05b4d21448b
liuyang <liuyang@ST-POM-LAB-NB58.SIMTech.a-star.edu.sg>
2019-12-05 17:56:43 +0800

    countdown

--- frontend/src/app/components/eventdetail/eventdetail.component.ts
+++ frontend/src/app/components/eventdetail/eventdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventService } from '../../services/event.service';
 
+
 @Component({
   selector: 'app-eventdetail',
   templateUrl: './eventdetail.component.html',
@@ -10,22 +11,97 @@ import { EventService } from '../../services/event.service';
 export class EventdetailComponent implements OnInit {
 
   eventId:string;
-  eventdata:any;
+  eventdata=null;
+
+  eventStartTime:string;
+
+ 
+
   constructor(private activeRoute:ActivatedRoute,
               private eventService:EventService) { }
 
-  ngOnInit() {
+ngOnInit() {
+
     this.activeRoute.paramMap.subscribe(params => {
       this.eventId = params.get("eid");
       this.getEvent(this.eventId)
     })
+    
   }
 
   getEvent(id){
     this.eventService.getEventById(id).subscribe((data)=>{
       console.log(data.results);
-      this.eventdata=data.results;
+      this.eventdata = data.results;
+      this.eventStartTime=this.getEventTime(this.eventdata);
+      setInterval(() => { this.update(this.eventStartTime) }, 1000);
     })
   }
+
+  getEventTime(event){
+    var date:string=event.dateFrom;
+    var timing:string=event.timingFrom;
+
+    var splitTiming=timing.split(" ")[0];
+    var unit=timing.split(" ")[1];
+
+    var hour=splitTiming.split(":")[0];
+    var min=splitTiming.split(":")[1];
+
+    if(unit=="PM"){
+      hour=(parseInt(hour)+12).toString();
+    }
+    if(hour.length<2){
+      hour="0"+hour;
+    }
+  
+    return date.substr(0,11)+hour+":"+min+":00"
+  }
+
+
+update(eventStartTime) {
+  var timer:any=document.getElementById("timer");
+  if ( timer.style.opacity !== 1 ) {
+    timer.style.opacity = 1;
+  }
+  
+  var currentDate:any = new Date();
+  var eventDate:any = new Date(eventStartTime);
+  var differenceDate = eventDate - currentDate;
+  
+  timer.days = document.querySelectorAll(".days .timer__number")[0];
+  timer.hours = document.querySelectorAll(".hours .timer__number")[0];
+  timer.minutes = document.querySelectorAll(".minutes .timer__number")[0];
+  timer.seconds = document.querySelectorAll(".seconds .timer__number")[0];
+
+  timer.days.innerHTML = this.getTimeRemaining(differenceDate,86400000, 1);
+  timer.hours.innerHTML = this.getTimeRemaining(differenceDate,3600000, 24);
+  timer.minutes.innerHTML = this.getTimeRemaining(differenceDate,60000, 60);
+  timer.seconds.innerHTML = this.getTimeRemaining(differenceDate,1000, 60);
+}
+
+
+
+/*
+* calculate remaining time based on a unit of time
+*/
+getTimeRemaining(difference, timeUnit, divisor ) {
+  var n;
+  if ( divisor == 1 ) {
+    n = Math.floor(difference / timeUnit );
+  }
+  else {
+    n = Math.floor((difference / timeUnit) % divisor );
+  }
+  
+  if ( String(n).length < 2 ) {
+    n = "0" + n;
+  }
+  return n;
+}
+
+
+
+
 
 }
